4d8cb858f0f7d9bb63edd8ea3c91509235b6ca44
Gilbert Jackson Ribeiro Dourado Alves <admin@imdocbrasil.com.br>
2025-11-03 22:32:52 +0000

    Try fixing this error: `Console Error: `legacyBehavior` is deprecated an

--- src/components/client-sidebar.tsx
+++ src/components/client-sidebar.tsx
@@ -55,16 +55,13 @@ export function ClientSidebar() {
     <SidebarMenu>
       {navItems.map((item) => (
         <SidebarMenuItem key={item.label}>
-           <Link href={item.href} passHref legacyBehavior>
+          <Link href={item.href}>
             <SidebarMenuButton
-              asChild
               isActive={getIsActive(item.href)}
               tooltip={item.label}
             >
-              <a>
-                <item.icon />
-                <span>{item.label}</span>
-              </a>
+              <item.icon />
+              <span>{item.label}</span>
             </SidebarMenuButton>
           </Link>
         </SidebarMenuItem>
