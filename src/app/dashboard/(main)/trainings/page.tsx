feed69cad6193da6680075f013ab547e6d481329
Gilbert Jackson Ribeiro Dourado Alves <admin@imdocbrasil.com.br>
2025-11-07 15:23:11 +0000

    Try fixing this error: `Build Error: Export initialTrainingsData doesn't

--- src/app/dashboard/(main)/trainings/page.tsx
+++ src/app/dashboard/(main)/trainings/page.tsx
@@ -1,3 +1,4 @@
+
 'use client'
 
 import { useState } from 'react'
@@ -60,7 +61,7 @@ export interface Training {
   workload: number // in hours
 }
 
-const initialTrainingsData: Training[] = [
+export const initialTrainingsData: Training[] = [
   {
     id: 'TRN-001',
     title: 'NR-35 - Trabalho em Altura',
