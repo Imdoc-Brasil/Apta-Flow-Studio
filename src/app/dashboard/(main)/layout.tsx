dd8477a89065ede7c2f5d80157ace8601273b803
Gilbert Jackson Ribeiro Dourado Alves <admin@imdocbrasil.com.br>
2025-11-08 19:21:59 +0000

    Ainda não resolveu

--- src/app/dashboard/(main)/layout.tsx
+++ src/app/dashboard/(main)/layout.tsx
@@ -18,11 +18,11 @@ export default function DashboardLayout({
 }) {
   return (
     <SidebarProvider>
-      <div className='flex min-h-screen w-full flex-col bg-muted/40'>
+      <div className='group flex min-h-screen w-full flex-row bg-muted/40'>
         <Sidebar>
           <DashboardNav />
         </Sidebar>
-        <div className='flex flex-col sm:pl-14 group-data-[state=expanded]:sm:pl-72 transition-all duration-300'>
+        <div className='flex flex-1 flex-col sm:pl-14 group-data-[state=expanded]:sm:pl-72 transition-all duration-300'>
           <header className='sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6'>
             <SidebarTrigger className='sm:flex hidden' />
             <Sheet>
