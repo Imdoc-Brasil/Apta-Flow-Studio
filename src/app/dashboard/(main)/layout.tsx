18cb509d1377c5f51ba4144961446bbd3fc2454c
Gilbert Jackson Ribeiro Dourado Alves <admin@imdocbrasil.com.br>
2025-11-10 16:37:52 +0000

    Try fixing this error: `Runtime Error: Error: useSidebar must be used wi

--- src/app/dashboard/(main)/layout.tsx
+++ src/app/dashboard/(main)/layout.tsx
@@ -1,7 +1,6 @@
 import { DashboardNav } from '@/components/dashboard-nav'
 import {
   Sidebar,
-  SidebarInset,
   SidebarProvider,
   SidebarTrigger,
 } from '@/components/ui/sidebar'
@@ -17,34 +16,36 @@ export default function DashboardLayout({
   children: React.ReactNode
 }) {
   return (
-    <div className='group flex min-h-screen w-full flex-row bg-muted/40'>
-      <Sidebar collapsible='icon'>
-        <DashboardNav />
-      </Sidebar>
-      <div className='flex flex-1 flex-col sm:pl-14 group-data-[state=expanded]:sm:pl-72 transition-all duration-300'>
-        <header className='sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6'>
-          <SidebarTrigger className='sm:flex hidden' />
-          <Sheet>
-            <SheetTrigger asChild>
-              <Button size='icon' variant='outline' className='sm:hidden'>
-                <PanelLeft className='h-5 w-5' />
-                <span className='sr-only'>Toggle Menu</span>
-              </Button>
-            </SheetTrigger>
-            <SheetContent side='left' className='sm:max-w-xs'>
-              <DashboardNav isSheet={true} />
-            </SheetContent>
-          </Sheet>
-          <Breadcrumb />
-          <div className='relative ml-auto flex-1 md:grow-0'>
-            {/* This can be a global search in the future */}
-          </div>
-          <UserNav />
-        </header>
-        <main className='flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6'>
-          {children}
-        </main>
+    <SidebarProvider>
+      <div className='group flex min-h-screen w-full flex-row bg-muted/40'>
+        <Sidebar collapsible='icon'>
+          <DashboardNav />
+        </Sidebar>
+        <div className='flex flex-1 flex-col sm:pl-14 group-data-[state=expanded]:sm:pl-72 transition-all duration-300'>
+          <header className='sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6'>
+            <SidebarTrigger className='sm:flex hidden' />
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button size='icon' variant='outline' className='sm:hidden'>
+                  <PanelLeft className='h-5 w-5' />
+                  <span className='sr-only'>Toggle Menu</span>
+                </Button>
+              </SheetTrigger>
+              <SheetContent side='left' className='sm:max-w-xs'>
+                <DashboardNav isSheet={true} />
+              </SheetContent>
+            </Sheet>
+            <Breadcrumb />
+            <div className='relative ml-auto flex-1 md:grow-0'>
+              {/* This can be a global search in the future */}
+            </div>
+            <UserNav />
+          </header>
+          <main className='flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6'>
+            {children}
+          </main>
+        </div>
       </div>
-    </div>
+    </SidebarProvider>
   )
 }
