a8afcd29e87223a8f75afd56bae49c0d46535db7
Gilbert Jackson Ribeiro Dourado Alves <admin@imdocbrasil.com.br>
2025-11-10 16:37:18 +0000

    File changes

--- src/app/dashboard/(main)/layout.tsx
+++ src/app/dashboard/(main)/layout.tsx
@@ -17,36 +17,34 @@ export default function DashboardLayout({
   children: React.ReactNode
 }) {
   return (
-    <SidebarProvider>
-      <div className='group flex min-h-screen w-full flex-row bg-muted/40'>
-        <Sidebar collapsible='icon'>
-          <DashboardNav />
-        </Sidebar>
-        <div className='flex flex-1 flex-col sm:pl-14 group-data-[state=expanded]:sm:pl-72 transition-all duration-300'>
-          <header className='sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6'>
-            <SidebarTrigger className='sm:flex hidden' />
-            <Sheet>
-              <SheetTrigger asChild>
-                <Button size='icon' variant='outline' className='sm:hidden'>
-                  <PanelLeft className='h-5 w-5' />
-                  <span className='sr-only'>Toggle Menu</span>
-                </Button>
-              </SheetTrigger>
-              <SheetContent side='left' className='sm:max-w-xs'>
-                <DashboardNav isSheet={true} />
-              </SheetContent>
-            </Sheet>
-            <Breadcrumb />
-            <div className='relative ml-auto flex-1 md:grow-0'>
-              {/* This can be a global search in the future */}
-            </div>
-            <UserNav />
-          </header>
-          <main className='flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6'>
-            {children}
-          </main>
-        </div>
+    <div className='group flex min-h-screen w-full flex-row bg-muted/40'>
+      <Sidebar collapsible='icon'>
+        <DashboardNav />
+      </Sidebar>
+      <div className='flex flex-1 flex-col sm:pl-14 group-data-[state=expanded]:sm:pl-72 transition-all duration-300'>
+        <header className='sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6'>
+          <SidebarTrigger className='sm:flex hidden' />
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button size='icon' variant='outline' className='sm:hidden'>
+                <PanelLeft className='h-5 w-5' />
+                <span className='sr-only'>Toggle Menu</span>
+              </Button>
+            </SheetTrigger>
+            <SheetContent side='left' className='sm:max-w-xs'>
+              <DashboardNav isSheet={true} />
+            </SheetContent>
+          </Sheet>
+          <Breadcrumb />
+          <div className='relative ml-auto flex-1 md:grow-0'>
+            {/* This can be a global search in the future */}
+          </div>
+          <UserNav />
+        </header>
+        <main className='flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6'>
+          {children}
+        </main>
       </div>
-    </SidebarProvider>
+    </div>
   )
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Space_Grotesk } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
 import { Toaster } from '@/components/ui/toaster'
+import { FirebaseClientProvider } from '@/firebase'
 
 const fontBody = Inter({
   subsets: ['latin'],
@@ -33,7 +34,7 @@ export default function RootLayout({
           fontHeadline.variable
         )}
       >
-        {children}
+        <FirebaseClientProvider>{children}</FirebaseClientProvider>
         <Toaster />
       </body>
     </html>
