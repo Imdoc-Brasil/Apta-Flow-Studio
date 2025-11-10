7bc3a99bf47a4e7a688c129525f26c29f83991e5
Gilbert Jackson Ribeiro Dourado Alves <admin@imdocbrasil.com.br>
2025-11-10 16:36:40 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9804515494-e1a53",
+  "appId": "1:759963794370:web:fb9698d247e0bf5d62345b",
+  "apiKey": "AIzaSyAzy52hOXCEcaoSNUwnOObmQbPG1HvvnXs",
+  "authDomain": "studio-9804515494-e1a53.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "759963794370"
+};
